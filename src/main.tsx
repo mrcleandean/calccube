4af5b5bedde744aca518fe5ce1f9cea72be229a9
Dean Kadri <deankadri@Minas-MacBook-Air.local>
2024-05-22 12:57:21 -0600

    added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import CubeProvider from './components/CubeProvider.tsx'
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Analytics />
     <CubeProvider>
       <App />
     </CubeProvider>
